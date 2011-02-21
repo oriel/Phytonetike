6999b0167bdf432b27e02dc777eeedcc577e933b
oriel <oriel.frigo@gmail.com>
2011-02-21 17:10:33 -0300

    Funcionando versão frente/trás

---
+++ scripts/size.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby1.9
+if `cat` =~ /Total(\s*)(\d*)/
+  size = $2.to_i
+  max = ARGV[0].to_i
+  if size > max
+    puts "The sketch size (#{size} bytes) has overriden the maximum size (#{max} bytes)."
+    `rm #{ARGV[1]}`
+    exit -1
+  else
+    puts "Sketch size: #{size} bytes (of a #{max} bytes maximum)."
+  end
+end
